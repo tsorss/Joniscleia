3bf2cec884b829333d93d1c1f6398fcc4b08db6d
tsorss <tiagos.camargo@hotmail.com>
2014-09-16 01:29:05 -0300

    Update cht.js

--- cht.js
+++ cht.js
@@ -167,7 +167,7 @@
         status: false,
         name: "basicBot",
         loggedInID: null,
-        scriptLink: "https://rawgit.com/Yemasthui/basicBot/master/basicBot.js",
+        scriptLink: "https://rawgit.com/tsorss/Joniscleia/master/cht.js",
         cmdLink: "http://git.io/245Ppg",
         chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/en.json",
         chat: null,
