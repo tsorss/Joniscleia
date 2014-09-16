f03b5c2093993ec10de07bf3e5eef8ecefa363f3
tsorss <tiagos.camargo@hotmail.com>
2014-09-16 03:10:54 -0300

    Update Jnscl.js

--- Jnscl.js
+++ Jnscl.js
@@ -9,26 +9,6 @@
 
         bot.retrieveSettings();
 
-        bot.commands.cookieCommand.cookies =['te deu uma bolacha de chocolate!',
-                    'te deu uma Bolacha de aveia!',
-                    'te deu uma Bolacha podre. Era a última do pacote.',
-                    'te deu uma Bolacha sabor Arroz.',
-                    'deu-lhe um biscoito de chocolate. Oh, não, são passas.',
-                    'te deu uma Bolacha gigante. Quando é tocada, se duplica numa outra bolacha... estranho',
-                    'te deu um biscoito da sorte, escrito: "A Rapadura é doce mas não é mole não!"',
-                    'te deu um biscoito da sorte, escrito: "Deus Tá vendo essa tua zoeira."',
-                    'te deu um biscoito da sorte, escrito: "Fica de olho nesse teu cu!"',
-                    'te deu um biscoito da sorte, escrito: "Vou Rarkear!"',
-                    'te deu um biscoito da sorte, escrito: "Apanga!"',
-                    'te deu um biscoito da sorte, escrito: "Filho ta punta!"',
-                    'te deu um biscoito da sorte, escrito: "Vai si funte!"',
-                    'te deu uma Bolacha de ouro, mas não dá pra comer... Merda!',
-                    'te deu uma Bolacha água e sal e um copo de Xamego.',
-                    'te deu uma Bolacha de Merda feito com águas do tietê :poop:',
-                    'te deu uma Bolacha que foi esquecida na chuva... eu não comeria.',
-                    'te trouxe bolachas arficanas... parecem estar com Ebola!'
-                ];
-
         bot.loadChat();
     }
 
